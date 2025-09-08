580ce24e3218645d249ed27607d2c00761b906f4
EleazarDev <eleazarjarquincv@gmail.com>
2025-09-08 15:15:08 -0600

    dead code

--- src/app/components/content/content.ts
+++ src/app/components/content/content.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { Profile } from "../profile/profile";
 import { Experiences } from "../experiences/experiences";
-import { Projects } from "../projects/projects";
 import { Education } from "../education/education";
 import { Articles } from "../articles/articles";
 import { Skills } from "../skills/skills";
 
 @Component({
   selector: 'app-content',
-  imports: [Profile, Experiences, Projects, Education, Articles, Skills],
+  imports: [Profile, Experiences, Education, Articles, Skills],
   templateUrl: './content.html',
   styleUrl: './content.css'
 })
